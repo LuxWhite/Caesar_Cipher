132c81c9781ac4ab9d0b703f041e6a62214c497e
Lux <44013652+LuxWhite@users.noreply.github.com>
2019-09-19 19:13:27 -0500

    Initial commit: Simple Caesar Cipher.

---
+++ src/caesar_Cipher.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+public class caesar_Cipher {
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to Caesar Cipher example");
+        System.out.println("Enter the word: ");
+        String text = input.nextLine();
+
+        int shift = 3;
+
+        System.out.println("__________________Caesar Cipher__________________");
+        System.out.println("The shift value: " + shift);
+        System.out.println("Encryption: " +encryption(text,shift));
+        System.out.println("_________________________________________________");
+
+        System.out.print("do you wish to Decrypt? y/n ");
+        Scanner ans = new Scanner(System.in);
+        String reply = ans.nextLine();
+
+        switch (reply.toUpperCase()){
+            case "Y":
+                StringBuffer encryption_Result = encryption(text,shift);
+                System.out.println("Decryption: "+encryption(encryption_Result.toString(),26-shift));
+                break;
+            case "N":
+                System.out.println("goodbye");
+                break;
+        }
+        System.out.println("______________________Fin_________________________");
+    }
+
+    //_______________________________________________________________________________________________________________________________
+    //Encryption Method (encoding the text)
+    public static StringBuffer encryption(String text, int n) {
+        StringBuffer encryption = new StringBuffer();
+
+        //Traverse each character in the string
+        for (int i = 0; i < text.length(); i++)
+        {
+            if (Character.isUpperCase(text.charAt(i)))
+            {
+                /*add shift to character of string
+                simultaneously adjusting ascii value
+                whether Uppercase or Lowercase letters.
+                */
+                char ch = (char) ((((int) text.charAt(i)) + n - 65) % 26 + 65);
+                encryption.append(ch);
+            } else
+                {
+                char ch = (char) ((((int) text.charAt(i)) + n - 97) % 26 + 97);
+                encryption.append((ch));
+            }
+        }
+        return encryption;
+    }
+} //End.
